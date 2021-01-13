5bfa1f84c6a4c8919044c3827b57d0fa5e6bb8a1
Dave Burke <dburke84@gmail.com>
2021-01-13 08:59:47 -0600

    Implement RPN stack

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+      stack: [],
+    }
+    this.handleInputchange = this.handleInputchange.bind(this)
+    this.handlePushClick = this.handlePushClick.bind(this)
+  }
+
+  handleInputchange(e) {
+    let value = Number(e.target.value)
+    if(isNaN(value)) {
+      value = ''
+    }
+    this.setState(state => ({
+      input: value,
+    }))
+  }
+
+  handlePushClick(e) {
+    this.setState(state => ({
+      stack: [...state.stack, this.state.input]
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Number:
+        <input type="text" onChange={this.handleInputchange} value={this.state.input}></input>
+        <button onClick={this.handlePushClick}>Push</button>
+        <ol>
+          {this.state.stack.map((n, index) => <li key={index}>{n}</li>)}
+        </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
