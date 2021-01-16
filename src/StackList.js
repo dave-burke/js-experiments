0f6e366d0a4829c6d30c8b68980af66ba8364c22
Dave Burke <dburke84@gmail.com>
2021-01-16 17:52:13 -0600

    Fix height of stack and scroll

--- src/StackList.js
+++ src/StackList.js
@@ -1,8 +1,21 @@
+import { useEffect, useRef } from 'react'
+
 function StackList(props) {
   const reversed = [...props.stack].reverse()
+  const container = useRef()
+
+  useEffect(() => {
+    const el = container.current
+    el.scrollTop = el.scrollHeight
+  })
+  
   return (
-    <div>
-      {reversed.filter(n => n !== '').map((n, index) => <p key={index}>{n}</p>)}
+    <div className="stackList" ref={container}>
+      <div className="stackList_spacer"></div>
+      {
+        reversed.filter(n => n !== '')
+        .map((n, index) => <div key={index}>{n}</div>)
+      }
     </div>
   )
 }
