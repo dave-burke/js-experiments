518318a2bf36babbafe3be8cd6be4a194986f2f3
Dave Burke <dburke84@gmail.com>
2021-01-16 12:14:41 -0600

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders last operation', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/Last operation was/i);
+  expect(text).toBeInTheDocument();
 });
