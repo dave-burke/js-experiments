b020a2b1af72734efa1d1e9642c65be65b3ffdb4
Dave Burke <dburke84@gmail.com>
2021-12-30 15:09:18 -0600

    Add TodoList

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
 import { ref } from 'vue'
 import Header from './components/Header.vue'
 import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
 
 const newTodo = ref('')
 
+const todos = ref([])
+
 function handleNewTodo() {
   console.log(`Save '${newTodo.value}`)
+  todos.value.push({id: new Date().getDate(), text: newTodo.value, status: 'ACTIVE'})
+  newTodo.value = ''
 }
 
 </script>
@@ -14,7 +19,7 @@ function handleNewTodo() {
 <template>
   <Header/>
   <TodoInput v-model="newTodo" @save="handleNewTodo"/>
-  <p>"{{newTodo}}"</p>
+  <TodoList :todos="todos"></TodoList>
 </template>
 
 <style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,10 @@
+<template>
+    <li>
+        <ul v-for="todo in todos">{{todo.text}}</ul>
+    </li>
+</template>
+<script setup>
+const props = defineProps({
+    todos: Array,
+})
+</script>
